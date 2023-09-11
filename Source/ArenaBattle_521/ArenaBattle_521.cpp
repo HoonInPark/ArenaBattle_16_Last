cefa957bdad46a337d2158af2b2387f7a8e8d5dc
HoonInPark <hooninpark@gmail.com>
2023-09-12 01:22:33 +0900

    1st commit of this branch

---
+++ Source/ArenaBattle_521/ArenaBattle_521.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ArenaBattle_521.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle_521, "ArenaBattle_521" );
---
+++ Source/ArenaBattle_521/ArenaBattle_521GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArenaBattle_521GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_521_API AArenaBattle_521GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
